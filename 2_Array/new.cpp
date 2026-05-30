7cd9bc46bff3e7776dd465348a34c4e6c02d57fb
Aaryam Dev <aaryam1@Aaryams-MacBook-Pro.local>
2026-05-31 04:00:14 +0530

    6

---
+++ 2_Array/new.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main()
+{
+
+    int* arr = new int[5];
+
+    
+    vector<int> v;
+    v.push_back(5);
+
+    string fname;
+
+    getline(cin, fname);
+
+    cout<<fname;
+
+    return 0;
+}
